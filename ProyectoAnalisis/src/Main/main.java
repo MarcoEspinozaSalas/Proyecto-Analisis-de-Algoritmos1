f6d65c1f006326baec246b27fdf77d7353453b37
Marco <marcoespinozasalas@gmail.com>
2020-05-30 01:38:40 -0600

    Documentacion interna del main y de procedimientos globales

--- ProyectoAnalisis/src/Main/main.java
+++ ProyectoAnalisis/src/Main/main.java
@@ -4,38 +4,40 @@
  * and open the template in the editor.
  */
 package Main;
+//importaciones
 
 import ArbolBinario.*;
-import ProcedimientosGlobales.Procedimientos;
+import ProcedimientosGlobales.*;
 
-import Grafos.MetodosGrafo;
+import Grafos.*;
 
 /**
  *
  * @author Lester Trejos
  */
 public class main {
-    //Globals
+
+    //llamadas a las instancias de cada clase
     static MetodosGrafo mg = new MetodosGrafo();
     static MetodosArbol metArbol = MetodosArbol.getInstance();
     static Procedimientos metProcedimientos = Procedimientos.getInstance();
     static MetodosGrafo metGrafo = MetodosGrafo.getInstance();
-    
-    public static void main(String[] args) {
-             
+
+    public static void main(String[] args) {//llamadas a los métodos de inserción
+
         medicionArbol();
-        
+
         medicionGrafo();
-        
+
     }
-    
-    static boolean llenarArbol(int i){
+
+    static boolean llenarArbol(int i) { //llamada al metodo de insercion
         Arbol aux = metArbol.raiz;
         boolean result = metArbol.insertar(aux);
         return result;
     }
-    
-    static void medicionArbol(){
+
+    static void medicionArbol() { //medicion de los datos del árbol binario
         System.out.println("------------Arbol--------------");
         int cant = 1000;
         //int cant = 5000;
@@ -43,67 +45,67 @@ public class main {
         //int cant = 100000;
         //int cant = 200000;
         //int cant = 500000;
-        for (int i = 0; i < cant; i++) {
+        for (int i = 0; i < cant; i++) {//comprueba que ningún valor se repita, ya que no se insetaría en el árbol
             boolean result = llenarArbol(i);
             if (!result) {
                 i--;
             }
         }
-           
+
         Arbol aux = metArbol.raiz;
-    
+
         System.out.println("--------Profundidad--------");
         metProcedimientos.InicioAPro = System.currentTimeMillis();
-        metProcedimientos.recorridoProfundidad(aux);
+        metProcedimientos.recorridoProfundidad(aux);//llamada al método de recorrido por profundidad
         metProcedimientos.FinAPro = System.currentTimeMillis();
-        metProcedimientos.tiempoAPro = ((metProcedimientos.FinAPro - metProcedimientos.InicioAPro)/1000);
-        System.out.println("Cantidad de comparaciones:" +" "+ metProcedimientos.cantidadDeComparacionesAPro);
-        System.out.println("Cantidad de asignaciones:" +" "+ metProcedimientos.cantidadDeAsignacionesAPro);
-        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" +" "+ metProcedimientos.tiempoAPro );
-        System.out.println("Cantidad de lineas:" +" "+ metProcedimientos.lineasAPro);
+        metProcedimientos.tiempoAPro = ((metProcedimientos.FinAPro - metProcedimientos.InicioAPro) / 1000);
+        System.out.println("Cantidad de comparaciones:" + " " + metProcedimientos.cantidadDeComparacionesAPro);
+        System.out.println("Cantidad de asignaciones:" + " " + metProcedimientos.cantidadDeAsignacionesAPro);
+        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" + " " + metProcedimientos.tiempoAPro);
+        System.out.println("Cantidad de lineas:" + " " + metProcedimientos.lineasAPro);
         System.out.println("_______________________________");
-        
+
         System.out.println("--------Anchura--------");
         metProcedimientos.InicioAAnc = System.currentTimeMillis();
-        metProcedimientos.recorridoAnchura(aux);
+        metProcedimientos.recorridoAnchura(aux);//llamada al método de recorrido por Anchura
         metProcedimientos.FinAAnc = System.currentTimeMillis();
-        metProcedimientos.tiempoAAnc = ((metProcedimientos.FinAAnc -metProcedimientos.InicioAAnc)/1000);
-        System.out.println("Cantidad de comparaciones:" +" "+ metProcedimientos.cantidadDeComparacionesAAnc);
-        System.out.println("Cantidad de asignaciones:" +" "+ metProcedimientos.cantidadDeAsignacionesAAnc);
-        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" +" "+ metProcedimientos.tiempoAAnc);
-        System.out.println("Cantidad de lineas:" +" "+ metProcedimientos.lineasAAnc);
+        metProcedimientos.tiempoAAnc = ((metProcedimientos.FinAAnc - metProcedimientos.InicioAAnc) / 1000);
+        System.out.println("Cantidad de comparaciones:" + " " + metProcedimientos.cantidadDeComparacionesAAnc);
+        System.out.println("Cantidad de asignaciones:" + " " + metProcedimientos.cantidadDeAsignacionesAAnc);
+        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" + " " + metProcedimientos.tiempoAAnc);
+        System.out.println("Cantidad de lineas:" + " " + metProcedimientos.lineasAAnc);
         System.out.println("_______________________________");
     }
-    
-     static void medicionGrafo(){
+
+    static void medicionGrafo() {//medicion de los datos del grafos
         System.out.println("------------Grafo--------------");
         mg.insertAutomatico(1000); 
         //mg.insertAutomatico(2500);
         //mg.insertAutomatico(3000); 
         //mg.insertAutomatico(4500);
-        //mg.insertAutomatico(5000); 
-        
+        //mg.insertAutomatico(5000);
+
         System.out.println("--------Profundidad--------");
         metProcedimientos.InicioGPro = System.currentTimeMillis();
-        metProcedimientos.profundidadGrafo(metGrafo.grafo);
+        metProcedimientos.profundidadGrafo(metGrafo.grafo);//llamada al método de recorrido por profundidad
         metProcedimientos.FinGPro = System.currentTimeMillis();
-        metProcedimientos.tiempoGPro = ((metProcedimientos.FinGPro - metProcedimientos.InicioGPro)/1000);
-        System.out.println("Cantidad de comparaciones:" +" "+ metProcedimientos.cantidadDeComparacionesGPro);
-        System.out.println("Cantidad de asignaciones:" +" "+ metProcedimientos.cantidadDeAsignacionesGPro);
-        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" +" "+  metProcedimientos.tiempoGPro );
-        System.out.println("Cantidad de lineas:" +" "+ metProcedimientos.lineasGPro);
+        metProcedimientos.tiempoGPro = ((metProcedimientos.FinGPro - metProcedimientos.InicioGPro) / 1000);
+        System.out.println("Cantidad de comparaciones:" + " " + metProcedimientos.cantidadDeComparacionesGPro);
+        System.out.println("Cantidad de asignaciones:" + " " + metProcedimientos.cantidadDeAsignacionesGPro);
+        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" + " " + metProcedimientos.tiempoGPro);
+        System.out.println("Cantidad de lineas:" + " " + metProcedimientos.lineasGPro);
         System.out.println("_______________________________");
-        
+
         System.out.println("--------Amplitud--------");
         System.out.println(mg.fuerteConexo());
         metProcedimientos.InicioGAmp = System.currentTimeMillis();
-        metProcedimientos.amplitudGrafo(metGrafo.grafo);
-        metProcedimientos.FinGAmp =  System.currentTimeMillis();
-        metProcedimientos.tiempoGAmp = ((metProcedimientos.FinGAmp -metProcedimientos.InicioGAmp)/1000);
-        System.out.println("Cantidad de comparaciones:" +" "+ metProcedimientos.cantidadDeComparacionesGAmp);
-        System.out.println("Cantidad de asignaciones:" +" "+ metProcedimientos.cantidadDeAsignacionesGAmp);
-        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" +" "+  metProcedimientos.tiempoGAmp);
-        System.out.println("Cantidad de lineas:" +" "+ metProcedimientos.lineasGAmp);
+        metProcedimientos.amplitudGrafo(metGrafo.grafo);//llamada al método de recorrido por amplitud
+        metProcedimientos.FinGAmp = System.currentTimeMillis();
+        metProcedimientos.tiempoGAmp = ((metProcedimientos.FinGAmp - metProcedimientos.InicioGAmp) / 1000);
+        System.out.println("Cantidad de comparaciones:" + " " + metProcedimientos.cantidadDeComparacionesGAmp);
+        System.out.println("Cantidad de asignaciones:" + " " + metProcedimientos.cantidadDeAsignacionesGAmp);
+        System.out.println("Cantidad de tiempo ejecucion en milisegundos:" + " " + metProcedimientos.tiempoGAmp);
+        System.out.println("Cantidad de lineas:" + " " + metProcedimientos.lineasGAmp);
         System.out.println("_______________________________");
-     }
+    }
 }
--- ProyectoAnalisis/src/ProcedimientosGlobales/Procedimientos.java
+++ ProyectoAnalisis/src/ProcedimientosGlobales/Procedimientos.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ProcedimientosGlobales;
+//importaciones
 
 import ArbolBinario.Arbol;
 import Grafos.*;
@@ -15,174 +16,171 @@ import java.util.Queue;
  * @author marco
  */
 public class Procedimientos {
-   //Globals
-   //--------------Arbol--------------
-   //Profundidad
-   public  static   int cantidadDeAsignacionesAPro = 0;
-   public  static  int cantidadDeComparacionesAPro = 0;
-   public static double FinAPro = 0;
-   public static double InicioAPro = 0;
-   public static double tiempoAPro = 0;
-   //Conteo Lineas
-   public static int lineasAPro;
-   //Anchura
-   public  static   int cantidadDeAsignacionesAAnc = 0;
-   public  static  int cantidadDeComparacionesAAnc = 0;
-   public static double FinAAnc = 0;
-   public static double InicioAAnc = 0;
-   public static double tiempoAAnc = 0;
-   //Conteo Lineas
-   public static int lineasAAnc;
-   
-   //--------------Grafo--------------
-   //Profundidad
-   public  static   int cantidadDeAsignacionesGPro = 0;
-   public  static  int cantidadDeComparacionesGPro = 0;
-   public static double FinGPro = 0;
-   public static double InicioGPro = 0;
-   public static double tiempoGPro = 0;
-   //Conteo Lineas
-   public static int lineasGPro;
-   //Amplitud
-   public  static   int cantidadDeAsignacionesGAmp = 0;
-   public  static  int cantidadDeComparacionesGAmp = 0;
-   public static double FinGAmp= 0;
-   public static double InicioGAmp = 0;
-   public static double tiempoGAmp = 0;
-   //Conteo Lineas
-   public static int lineasGAmp;
-   
-   
-   
+    //Globals
+    //--------------Arbol--------------
+    //Variables para recorrido de porfundidad del árbol binario
+
+    public static int cantidadDeAsignacionesAPro = 0;
+    public static int cantidadDeComparacionesAPro = 0;
+    public static double FinAPro = 0;
+    public static double InicioAPro = 0;
+    public static double tiempoAPro = 0;
+    //Conteo Lineas
+    public static int lineasAPro;
+    //Variables para recorrido de anchura del árbol binario
+    public static int cantidadDeAsignacionesAAnc = 0;
+    public static int cantidadDeComparacionesAAnc = 0;
+    public static double FinAAnc = 0;
+    public static double InicioAAnc = 0;
+    public static double tiempoAAnc = 0;
+    //Conteo Lineas
+    public static int lineasAAnc;
+
+    //--------------Grafo--------------
+    //Variables para recorrido de porfundidad del grafo
+    public static int cantidadDeAsignacionesGPro = 0;
+    public static int cantidadDeComparacionesGPro = 0;
+    public static double FinGPro = 0;
+    public static double InicioGPro = 0;
+    public static double tiempoGPro = 0;
+    //Conteo Lineas
+    public static int lineasGPro;
+    //Variables para recorrido de amplitud del grafo
+    public static int cantidadDeAsignacionesGAmp = 0;
+    public static int cantidadDeComparacionesGAmp = 0;
+    public static double FinGAmp = 0;
+    public static double InicioGAmp = 0;
+    public static double tiempoGAmp = 0;
+    //Conteo Lineas
+    public static int lineasGAmp;
+
+    //creación de la instancia, singleton
     public static Procedimientos instance = null;
-        public static Procedimientos getInstance(){
-            if(instance == null){
-                instance = new Procedimientos();
-            }
-            return instance;
+
+    public static Procedimientos getInstance() {
+        if (instance == null) {
+            instance = new Procedimientos();
         }
-        
+        return instance;
+    }
+    //recorrido recursivo por profundidad del árbol binario, también llamado recorrido en preorden
+
     public void recorridoProfundidad(Arbol aux) {
         cantidadDeComparacionesAPro++;
         lineasAPro++;
-        if (aux == null) {
+        if (aux == null) {//condición de parada
             lineasAPro++;
             return;
         }
         lineasAPro++;
-        System.out.println("Nodo: "+ aux.num);
+        System.out.println("Nodo: " + aux.num);
         cantidadDeAsignacionesAPro++;
         lineasAPro++;
-        recorridoProfundidad(aux.izq);
+        recorridoProfundidad(aux.izq);//recursión
         cantidadDeAsignacionesAPro++;
         lineasAPro++;
-        recorridoProfundidad(aux.der);
+        recorridoProfundidad(aux.der);//recursión
     }
-    
-    public void recorridoAnchura(Arbol aux)  
-    { 
+
+    //recorrido iterativo por anchura del árbol binario
+    public void recorridoAnchura(Arbol aux) {
         lineasAAnc++;
-        Queue<Arbol> queue = new LinkedList<>(); 
+        Queue<Arbol> queue = new LinkedList<>();//se utiliza cola en vez de pila
         cantidadDeComparacionesAAnc++;
         lineasAAnc++;
-        queue.add(aux);
+        queue.add(aux);//se guarda en cola
         lineasAAnc++;
-        while (!queue.isEmpty())  
-        { 
+        while (!queue.isEmpty()) //pregunta si la cola está vacía
+        {
             cantidadDeComparacionesAAnc++;
             cantidadDeAsignacionesAAnc++;
             lineasAAnc++;
-            Arbol tempNode = queue.poll();
+            Arbol tempNode = queue.poll();// devuelve y elimina el elemento en la parte frontal del contenedor.
             lineasAAnc++;
-            System.out.println("Nodo: " + tempNode.num); 
-  
-            /*Enqueue left child */
+            System.out.println("Nodo: " + tempNode.num);
+
+            /*pone en cola el hijo izquierdo */
             cantidadDeComparacionesAAnc++;
             lineasAAnc++;
-            if (tempNode.izq != null) { 
+            if (tempNode.izq != null) {
                 cantidadDeAsignacionesAAnc++;
                 lineasAAnc++;
-                queue.add(tempNode.izq); 
-            } 
-  
-            /*Enqueue right child */
-            cantidadDeComparacionesAAnc++;
-            lineasAAnc++;
-            if (tempNode.der != null) { 
-                cantidadDeAsignacionesAAnc++;
-                lineasAAnc++;
-                queue.add(tempNode.der); 
-            } 
-        }
-             
-    } 
-           
-   public void profundidadGrafo(Vertice grafo)//metodo que imprime el inicio en profundidadGrafo
-        {
-            cantidadDeComparacionesGPro++;
-            lineasGPro++; 
-            if ((grafo == null)|(grafo.visitado == true)){
-                lineasGPro++; 
-                return;
+                queue.add(tempNode.izq); //se guarda en cola
             }
-            else{
-                cantidadDeAsignacionesGPro++;
-                lineasGPro++; 
-                grafo.visitado = true;
-                System.out.println("Vertice:" +grafo.num);
-                cantidadDeAsignacionesGPro++;
-                lineasGPro++; 
-                Arco aux = grafo.subListaArcos;
-                lineasGPro++; 
-                while (aux != null){//cambiar los outPrint por listModel.addElement
-                    cantidadDeComparacionesGPro++;
-                    lineasGPro++; 
-                    cantidadDeAsignacionesGPro++;
-                    lineasGPro++; 
-                    profundidadGrafo(aux.destino);
-                    cantidadDeAsignacionesGPro++;
-                    lineasGPro++; 
-                    aux = aux.sigA;
-                }
+
+            /*pone en cola el hijo derecho */
+            cantidadDeComparacionesAAnc++;
+            lineasAAnc++;
+            if (tempNode.der != null) {
+                cantidadDeAsignacionesAAnc++;
+                lineasAAnc++;
+                queue.add(tempNode.der); //se guarda en cola
             }
         }
 
-   public void amplitudGrafo(Vertice grafo)// metodo para imprimir el inicio en amplitudGrafo
-        {
-            cantidadDeComparacionesGAmp++;
-            lineasGAmp++;
-            if (grafo == null){
-                lineasGAmp++;
-                System.out.println("No hay grafo");
-            }
-            else
-            {
-                cantidadDeAsignacionesGAmp++;
-                lineasGAmp++;
-                Vertice temp = grafo;
-                lineasGAmp++;
-                while (temp != null)
-                {
-                    cantidadDeComparacionesGAmp++;
-                    lineasGAmp++;
-                    System.out.println("Vertice: " + temp.num);
-                    cantidadDeAsignacionesGAmp++;
-                    lineasGAmp++;
-                    Arco aux = temp.subListaArcos;
-                    lineasGAmp++;
-                    while (aux != null)
-                    {
-                       cantidadDeComparacionesGAmp++;
-                       //System.out.println("Destino: " + aux.destino.num);
-                       cantidadDeAsignacionesGAmp++;
-                       lineasGAmp++;
-                       aux = aux.sigA;
-                    }
-                    //System.out.println("-----------");
-                    cantidadDeAsignacionesGAmp++;
-                    lineasGAmp++;
-                    temp = temp.sigV;
-                }
+    }
+
+    public void profundidadGrafo(Vertice grafo)//método recursivo que imprime el inicio en profundidad de un Grafo
+    {
+        cantidadDeComparacionesGPro++;
+        lineasGPro++;
+        if ((grafo == null) | (grafo.visitado == true)) {//condiciones de parada
+            lineasGPro++;
+            return;
+        } else {
+            cantidadDeAsignacionesGPro++;
+            lineasGPro++;
+            grafo.visitado = true;//mueve marca de false a true
+            System.out.println("Vertice:" + grafo.num);
+            cantidadDeAsignacionesGPro++;
+            lineasGPro++;
+            Arco aux = grafo.subListaArcos;
+            lineasGPro++;
+            while (aux != null) {//se podría imprimir la información de los vértices y sus arcos si se quiere
+                cantidadDeComparacionesGPro++;
+                lineasGPro++;
+                cantidadDeAsignacionesGPro++;
+                lineasGPro++;
+                profundidadGrafo(aux.destino);//recursión
+                cantidadDeAsignacionesGPro++;
+                lineasGPro++;
+                aux = aux.sigA;
             }
         }
+    }
+
+    public void amplitudGrafo(Vertice grafo)// método para imprimir el inicio en amplitud de un Grafo
+    {
+        cantidadDeComparacionesGAmp++;
+        lineasGAmp++;
+        if (grafo == null) {//si no hay vértices
+            lineasGAmp++;
+            System.out.println("No hay grafo");
+        } else {
+            cantidadDeAsignacionesGAmp++;
+            lineasGAmp++;
+            Vertice temp = grafo;//se posiciona en el vértice
+            lineasGAmp++;
+            while (temp != null)//condición para ir al siguiente vértice
+            {
+                cantidadDeComparacionesGAmp++;
+                lineasGAmp++;
+                System.out.println("Vertice: " + temp.num);
+                cantidadDeAsignacionesGAmp++;
+                lineasGAmp++;
+                Arco aux = temp.subListaArcos;//posicionarse en los arcos del vértice
+                lineasGAmp++;
+                while (aux != null)//condición para ir al siguiente arco
+                {
+                    cantidadDeComparacionesGAmp++;
+                    cantidadDeAsignacionesGAmp++;
+                    lineasGAmp++;
+                    aux = aux.sigA;
+                }
+                cantidadDeAsignacionesGAmp++;
+                lineasGAmp++;
+                temp = temp.sigV;
+            }
+        }
+    }
 }
